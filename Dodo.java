3d07ae958dd9b8c220a8568fadd2bfe56218c013
mrSymon <42111231+mrSymon@users.noreply.github.com>
2018-08-06 05:47:33 +0300

    Add files via upload

---
+++ Dodo.java
@@ -0,0 +1,15 @@
+package Survile;
+
+public class Dodo {
+
+  public static void main(String[] args)   
+throws java.io.IOException{
+   
+   char ch = 'a';
+   while (ch <= 'z'){
+     System.out.print(ch + "|");
+     ch++;
+   }
+   
+  }
+}
